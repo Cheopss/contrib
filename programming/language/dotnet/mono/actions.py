87ab97f578f29211d462ff1250079ab908793d9a
cheopss <aydinatmaca@gmail.com>
2018-12-27 17:24:55 +0300

    version bump-dep add-mono-data-postgresql del

--- programming/language/dotnet/mono/actions.py
+++ programming/language/dotnet/mono/actions.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 # Licensed under the GNU General Public License, version 3.
@@ -6,42 +6,15 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
-import glob
-import os.path
-
-shelltools.export("LC_ALL", "C")
-
 def setup():
-    autotools.autoreconf("-fi")
-
-    autotools.configure("--disable-quiet-build \
-                         --disable-system-aot \
-                         --disable-static \
-                         --with-mcs-docs=no")
+    autotools.configure()
 
 def build():
-    autotools.make()
-
-    shelltools.cd("mcs/jay")
     autotools.make()
 
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    shelltools.cd("mcs/jay")
-    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
-    shelltools.cd("..")
-
-    # These work on Windows only
-    for i in glob.glob("%s/usr/lib/mono/*/Mono.Security.Win32*" % get.installDIR()):
-        x = i.split(get.installDIR())[-1]
-        if os.path.isdir(x):
-            pisitools.removeDir(x)
-            continue
-
-        pisitools.remove(x)
-
-    pisitools.dodoc("AUTHORS")
+    pisitools.dodoc("COPYING.LIB", "LICENSE", "NEWS")
